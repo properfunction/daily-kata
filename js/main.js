d1d81845046bcb40a0545748865a1aa231b83008
properfunction <111qazxccv@gmail.com>
2022-04-02 19:49:54 +1000

    add second code challenge

--- js/main.js
+++ js/main.js
@@ -4,3 +4,10 @@
 function sumMix(x) {
     return x.map(a => +a).reduce((a, b) => a + b);
 }
+
+// get the sum of two arrays...actually the sum of all their elements
+
+function arrayPlusArray(arr1, arr2) {
+    return arr1.concat(arr2).reduce((a, b) => a + b);
+}
+
