31028bb35729ec647a7c14cbe205130b2601ce62
properfunction <111qazxccv@gmail.com>
2022-04-20 19:42:28 +1000

    add summation

---
+++ js/cw200422.js
@@ -0,0 +1,10 @@
+// Write a program that finds the summation of every number from 1 to num. 
+
+var summation = function (num) {
+  let result = 0;
+  for (var i = 1; i <= num; i++) {
+    result += i;
+  }
+  
+  return result;
+}
