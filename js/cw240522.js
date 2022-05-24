62c6cfae619e0a7a40bd55766c48abe2aa42a451
properfunction <111qazxccv@gmail.com>
2022-05-24 13:56:24 +1000

    add min and max values

---
+++ js/cw240522.js
@@ -0,0 +1,5 @@
+// Your task is to make two functions (max and min) that receive a list of integers as input and return, respectively, 
+// the largest and lowest number in that list. You may consider that there will not be any empty arrays/vectors.
+
+const min = (list) => Math.min(...list);
+const max = (list) => Math.max(...list);
