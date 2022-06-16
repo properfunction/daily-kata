19f3e2e63a8e96757b4762c23c6ba9c32f34b6e4
properfunction <111qazxccv@gmail.com>
2022-06-16 15:28:24 +1000

    add sorted list

---
+++ js/cw160622.js
@@ -0,0 +1,7 @@
+//  Sort the inventors by years lived
+    const oldest = inventors.sort(function(a, b) {
+      const lastInventor = a.passed - a.year;
+      const nextInventor = b.passed - b.year;
+      return lastInventor > nextInventor ? -1 : 1;
+    });
+    console.log(oldest);
