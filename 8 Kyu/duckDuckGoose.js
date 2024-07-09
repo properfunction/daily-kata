d9e2eafac16b610999e08324809168fc3f02f3de
properfunction <111qazxccv@gmail.com>
2024-07-09 21:36:23 +1000

    Return name property of given object

---
+++ 8 Kyu/duckDuckGoose.js
@@ -0,0 +1,6 @@
+// Given an array of Player objects and an index (1-based), return the name of the chosen Player
+// (name is a property of Player objects, e.g Player.name)        
+
+function duckDuckGoose(players, goose) {
+  return players[(goose - 1) % players.length].name
+}
