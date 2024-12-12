35a954cbdc98c7cfdde24351dc7a65b653219492
properfunction <111qazxccv@gmail.com>
2024-12-12 16:46:50 +1000

    return a new array which doubles the value of each integer in original array

---
+++ 8 Kyu/lostWithoutMap121224.js
@@ -0,0 +1,10 @@
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+
+// [1, 2, 3] --> [2, 4, 6]
+
+function maps(x){
+  const newArray = x.map((int) => int*2)
+  return newArray
+}
